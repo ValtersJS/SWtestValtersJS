5e35e28b2df2d69e89a97d93c2618b566175cdfd
ValtersJS <valters.sakarnis@gmail.com>
2022-03-07 21:01:10 +0200

    shortened path

--- Index.php
+++ Index.php
@@ -12,7 +12,7 @@ use App\ListPage\ListPageLogic\TableGen;
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style>
-    <?php include 'C:\xampp\htdocs\swtest_v1\scss\List\ProdList.css'; ?>
+    <?php include 'scss\List\ProdList.css'; ?>
   </style>
   <title>Product List</title>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
